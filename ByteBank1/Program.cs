77bf582785d83e3101ef709671ffd00ac4874336
Fernando Bruno <fernandobrunoslima@gmail.com>
2023-01-06 15:17:23 -0300

    Insersão de solicitação de senhas para fazer transações bancárias

--- ByteBank1/Program.cs
+++ ByteBank1/Program.cs
@@ -21,8 +21,8 @@ namespace ByteBank1 {
         static void ShowSubMenu()
         {
             Console.WriteLine("1 - Efetuar depósito");
-            Console.WriteLine("2 - Efetuar transferência");
-            Console.WriteLine("3 - Efetuar saque");
+            Console.WriteLine("2 - Efetuar saque");
+            Console.WriteLine("3 - Efetuar transferência");
             Console.WriteLine("0 - Voltar para o menu principal: ");
             Console.WriteLine();
             Console.Write("Digite a opção desejada: ");
@@ -85,12 +85,16 @@ namespace ByteBank1 {
         }
 
 
-        static void MakeDeposit(List<string> cpfs, List<string> holders, List<double> balances)
+        static void MakeDeposit(List<string> cpfs, List<string> holders, List<string> passwords, List<double> balances)
         {
             Console.Write("Digite o cpf: ");
             string cpfTyped = Console.ReadLine();
             int indexFoundByCpf = cpfs.FindIndex(cpf => cpf == cpfTyped);
+            Console.Write("Digite a senha: ");
+            string passwordTyped = Console.ReadLine();
+
             double amount = 0;
+
             if (indexFoundByCpf == -1)
             {
                 Console.WriteLine("Não foi possível realizar o depósito");
@@ -98,80 +102,107 @@ namespace ByteBank1 {
             }
             else
             {
-                Console.Write("Digite o valor para depósito: ");
-                amount = double.Parse(Console.ReadLine());
-                balances[indexFoundByCpf] += amount;
-                Console.WriteLine($"Depósito realizado: {amount:F2}");
-                Console.WriteLine($"Saldo atual: {balances[indexFoundByCpf]:F2}");
+                if (passwordTyped == passwords[indexFoundByCpf])
+                {
+                    Console.Write("Digite o valor para depósito: ");
+                    amount = double.Parse(Console.ReadLine());
+                    balances[indexFoundByCpf] += amount;
+                    Console.WriteLine($"Depósito realizado: {amount:F2}");
+                    Console.WriteLine($"Saldo atual: {balances[indexFoundByCpf]:F2}");
+                } else
+                {
+                    Console.WriteLine("Senha Inválida. Tente novamente");
+                }
             }
 
 
         }
-        static void MakeWithdrawal(List<string> cpfs, List<string> holders, List<double> balances)
+        static void MakeWithdrawal(List<string> cpfs, List<string> holders, List<string> passwords, List<double> balances)
         {
             Console.Write("Digite o cpf: ");
             string cpfTyped = Console.ReadLine();
             int indexFoundByCpf = cpfs.FindIndex(cpf => cpf == cpfTyped);
+            Console.Write("Digite a senha: ");
+            string passwordTyped = Console.ReadLine();
+
             double amount = 0;
 
-            if (indexFoundByCpf == -1)
+            if (passwordTyped == passwords[indexFoundByCpf])
             {
-                Console.WriteLine("Não é possível efetuar saque");
-                Console.WriteLine("MOTIVO: conta não encontrada.");
-            }
-            else
-            {
-                Console.Write("Digite o valor para saque: ");
-                amount = double.Parse(Console.ReadLine());
-            }
+                if (indexFoundByCpf == -1)
+                {
+                    Console.WriteLine("Não é possível efetuar saque");
+                    Console.WriteLine("MOTIVO: conta não encontrada.");
+                }
+                else
+                {
+                    Console.Write("Digite o valor para saque: ");
+                    amount = double.Parse(Console.ReadLine());
+                }
 
-            if (balances[indexFoundByCpf] >= amount)
-            {
-                balances[indexFoundByCpf] -= amount;
-                Console.WriteLine($"Saque efetuado: {amount:F2}");
-                Console.WriteLine($"Saldo atual: {balances[indexFoundByCpf]:F2}");
+                if (balances[indexFoundByCpf] >= amount)
+                {
+                    balances[indexFoundByCpf] -= amount;
+                    Console.WriteLine($"Saque efetuado: {amount:F2}");
+                    Console.WriteLine($"Saldo atual: {balances[indexFoundByCpf]:F2}");
+                }
+                else
+                {
+                    Console.WriteLine("Saque não realizado");
+                    Console.WriteLine("MOTIVO: saldo insuficiente");
+                }
             }
             else
             {
-                Console.WriteLine("Saque não realizado");
-                Console.WriteLine("MOTIVO: saldo insuficiente");
+                Console.WriteLine("Senha Inválida. Tente novamente");
             }
         }
-        static void MakeTransfer(List<string> cpfs, List<string> titulares, List<double> saldos)
+        static void MakeTransfer(List<string> cpfs, List<string> holders, List<string> passwords, List<double> balances)
         {
             Console.Write("Digite o cpf do usuário da conta: ");
             string cpfTyped = Console.ReadLine();
             int indexFoundByCpf = cpfs.FindIndex(cpf => cpf == cpfTyped);
-            Console.Write("Digite o cpf da conta do credor: ");
-            string payeeCpf = Console.ReadLine();
-            int payeeIndex = cpfs.FindIndex(cpf => cpf == payeeCpf);
+            Console.Write("Digite a senha: ");
+            string passwordTyped = Console.ReadLine();
 
-            double amount = 0;
-            if (indexFoundByCpf == -1 || payeeIndex == -1)
+            if (passwordTyped == passwords[indexFoundByCpf])
             {
-                Console.WriteLine("Transferência não realizada");
-                Console.WriteLine("MOTIVO: conta(s) não encontrada(s).");
+
+                Console.Write("Digite o cpf da conta do credor: ");
+                string payeeCpf = Console.ReadLine();
+                int payeeIndex = cpfs.FindIndex(cpf => cpf == payeeCpf);
+
+                double amount = 0;
+                if (indexFoundByCpf == -1 || payeeIndex == -1)
+                {
+                    Console.WriteLine("Transferência não realizada");
+                    Console.WriteLine("MOTIVO: conta(s) não encontrada(s).");
+                }
+                else
+                {
+                    Console.Write("Digite o valor: ");
+                    amount = double.Parse(Console.ReadLine());
+                }
+
+                if (balances[indexFoundByCpf] < amount)
+                {
+                    Console.WriteLine("Transferência não realizada");
+                    Console.WriteLine("MOTIVO: saldo insuficiente.");
+                }
+                else
+                {
+                    balances[indexFoundByCpf] -= amount;
+                    balances[payeeIndex] += amount;
+                    Console.WriteLine($"Transferencia Realizada no valor de {amount:F2}");
+                }
             }
             else
             {
-                Console.Write("Digite o valor: ");
-                amount = double.Parse(Console.ReadLine());
-            }
-
-            if (saldos[indexFoundByCpf] < amount)
-            {
-                Console.WriteLine("Transferência não realizada");
-                Console.WriteLine("MOTIVO: saldo insuficiente.");
-            }
-            else
-            {
-                saldos[indexFoundByCpf] -= amount;
-                saldos[payeeIndex] += amount;
-                Console.WriteLine($"Transferencia Realizada no valor de {amount:F2}");
+                Console.WriteLine("Senha Inválida. Tente novamente");
             }
         }
 
-        static void bankTransactions(List<string> cpfs, List<string> holders, List<double> balances)
+        static void bankTransactions(List<string> cpfs, List<string> holders, List<string> passwords, List<double> balances)
         {
 
             int option;
@@ -183,13 +214,13 @@ namespace ByteBank1 {
                 switch (option)
                 {
                     case 1:
-                        MakeDeposit(cpfs, holders, balances);
+                        MakeDeposit(cpfs, holders, passwords, balances);
                         break;
                     case 2:
-                        MakeWithdrawal(cpfs, holders, balances);
+                        MakeWithdrawal(cpfs, holders, passwords, balances);
                         break;
                     case 3:
-                        MakeTransfer(cpfs, holders, balances);
+                        MakeTransfer(cpfs, holders, passwords, balances);
                         break;
                     
                 }
@@ -238,7 +269,7 @@ namespace ByteBank1 {
                         ShowAccumulatedValue(balances);
                         break;
                     case 6:
-                        bankTransactions(cpfs, holders, balances);
+                        bankTransactions(cpfs, holders, passwords, balances);
                         break;
                 }
 
